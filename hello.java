67631b0a74987369cf631640dd215762902df6d5
NightFoxFR <93703098+NightFoxFR@users.noreply.github.com>
2021-11-04 14:09:11 +0100

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,5 @@ public class hello {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World Jonckiere jawed c'est moi !");
-		git config --global user.email "adresse mail Git";
-		git config --global user.name "profil Git";
 	}
 }
